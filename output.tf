ec895932a70e398e142014b415c9c9a04fc8e65d
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-11 10:44:58 +0200

    update

---
+++ output.tf
@@ -0,0 +1,44 @@
+
+# ----------------------
+# VPC Outputs
+# ----------------------
+output "vpc_id" {
+  description = "ID of the main VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = [for s in aws_subnet.public : s.id]
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = [for s in aws_subnet.private : s.id]
+}
+
+# ----------------------
+# Internet Gateway / NAT Outputs
+# ----------------------
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.igw.id
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = aws_nat_gateway.nat.id
+}
+
+# ----------------------
+# Security Groups
+# ----------------------
+output "lambda_security_group_id" {
+  description = "ID of the Lambda security group"
+  value       = aws_security_group.lambda_sg.id
+}
+
+output "rds_security_group_id" {
+  description = "ID of the RDS security group"
+  value       = aws_security_group.rds_sg.id
+}
